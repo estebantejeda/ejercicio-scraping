b7a69fe62d59024f6e326e026fd2f59c862b6b45
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-02-16 01:10:35 -0300

    Leve cambio en el main

--- src/app.ts
+++ src/app.ts
@@ -5,55 +5,53 @@ import Csv from "./helpers/Csv";
 import TasaPobreza from "./Interfaces/TasaPobreza";
 import PoblacionCarente from "./Interfaces/PoblacionCarente";
 
-class App {
-    static async main(){
-        const URL = "https://www.bcn.cl/siit/reportescomunales/comunas_v.html?anno=2020&idcom=14101";
-        const bcn = await Getter.build(URL);
-        const html = bcn.html;
-        bcn.save();
+async function main() {
+    const URL = "https://www.bcn.cl/siit/reportescomunales/comunas_v.html?anno=2020&idcom=14101";
+    const bcn = await Getter.build(URL);
+    const html = bcn.html;
+    bcn.save();
 
-        const $ = cheerio.load(html);
+    const $ = cheerio.load(html);
 
-        let tasPobArr: TasaPobreza[] = [];
-        let poblCarArr: PoblacionCarente[] = [];
+    let tasPobArr: TasaPobreza[] = [];
+    let poblCarArr: PoblacionCarente[] = [];
 
-        $("#v-pills-2").each((_idx, elem) => {
-            const tables = $(elem).find("table");
+    $("#v-pills-2").each((_idx, elem) => {
+        const tables = $(elem).find("table");
 
-            tasPobArr = $(tables[0]).find("tr").map((_idx, elem) => {
-                const tds = $(elem).find("td");
-                const uniTer = $(tds[0]).text().trim();
-                const porIng = $(tds[1]).text().trim();
-                const multDim = $(tds[2]).text().trim();
-                if(uniTer === "" || porIng === "" || multDim === "") return;
-                return {
-                    unidadTerritorial: uniTer, 
-                    porIngresos: porIng, 
-                    multidimensional: multDim
-                };
-            }).get();
+        tasPobArr = $(tables[0]).find("tr").map((_idx, elem) => {
+            const tds = $(elem).find("td");
+            const uniTer = $(tds[0]).text().trim();
+            const porIng = $(tds[1]).text().trim();
+            const multDim = $(tds[2]).text().trim();
+            if (uniTer === "" || porIng === "" || multDim === "") return;
+            return {
+                unidadTerritorial: uniTer,
+                porIngresos: porIng,
+                multidimensional: multDim
+            };
+        }).get();
 
-            poblCarArr = $(tables[1]).find("tr").map((_idx, elem) => {
-                const tds = $(elem).find("td");
-                const uniTer = $(tds[0]).text().trim();
-                const perCar = $(tds[1]).text().trim();
-                const hogHac = $(tds[2]).text().trim();
-                if(uniTer === "" || perCar === "" || hogHac === "") return;
-                return {
-                    unidadTerritorial: uniTer, 
-                    personasCarentes: perCar, 
-                    hogaresHacinados: hogHac
-                };
-            }).get();
+        poblCarArr = $(tables[1]).find("tr").map((_idx, elem) => {
+            const tds = $(elem).find("td");
+            const uniTer = $(tds[0]).text().trim();
+            const perCar = $(tds[1]).text().trim();
+            const hogHac = $(tds[2]).text().trim();
+            if (uniTer === "" || perCar === "" || hogHac === "") return;
+            return {
+                unidadTerritorial: uniTer,
+                personasCarentes: perCar,
+                hogaresHacinados: hogHac
+            };
+        }).get();
 
-        });
+    });
 
-        const tasPobArrCsv = new Csv(tasPobArr);
-        const poblCarArrCsv = new Csv(poblCarArr);
-        tasPobArrCsv.save("tasa");
-        poblCarArrCsv.save("poblacion");
-        
-    }
+    const tasPobArrCsv = new Csv(tasPobArr);
+    const poblCarArrCsv = new Csv(poblCarArr);
+    tasPobArrCsv.save("tasa");
+    poblCarArrCsv.save("poblacion");
+
 }
 
-App.main();
+main();
