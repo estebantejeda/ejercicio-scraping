ebdb6260a38a8bc10142f48aa3709451a63600c5
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-02-23 12:14:56 -0300

    Creada clases para conectar a base de datos mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import cheerio from "cheerio";
 import Getter from "./helpers/Getter";
 import Csv from "./helpers/Csv";
-import TasaPobreza from "./interfaces/TasaPobreza";
-import PoblacionCarente from "./interfaces/PoblacionCarente";
+import ITasaPobreza from "./interfaces/TasaPobreza";
+import IPoblacionCarente from "./interfaces/PoblacionCarente";
 import extractionType from "./enums/ExtractionType";
+import Database from "./config/Database";
+import TasaPobreza from "./Models/TasaPobreza";
+import PoblacionCarente from "./Models/PoblacionCarente";
 
 async function main() {
+
+    await Database.connect();
+
     const URL = "https://www.bcn.cl/siit/reportescomunales/comunas_v.html?anno=2020&idcom=14101";
     const bcn = await Getter.build(URL);
     const html = bcn.html;
@@ -13,14 +19,14 @@ async function main() {
 
     const $ = cheerio.load(html);
 
-    let tasPobArr: TasaPobreza[] = [];
-    let poblCarArr: PoblacionCarente[] = [];
+    let tasPobArr: ITasaPobreza[] = [];
+    let poblCarArr: IPoblacionCarente[] = [];
     const date = new Date();
 
     $("#v-pills-2").each((_idx, elem) => {
         const tables = $(elem).find("table");
 
-        tasPobArr = $(tables[0]).find("tr").map((_idx, elem) => {
+        tasPobArr = $(tables[0]).find("tr").map(async (_idx, elem) => {
             const tds = $(elem).find("td");
             const uniTer = $(tds[0]).text().trim();
             const porIng = $(tds[1]).text().trim();
@@ -28,17 +34,20 @@ async function main() {
             if (uniTer === "" || porIng === "" || multDim === "") return;
             const porIngresos = stringToNumber(porIng);
             const multidimensional = stringToNumber(multDim);
-            return {
+            const data: ITasaPobreza = {
                 unidadTerritorial: uniTer,
                 porIngresos,
                 multidimensional,
                 date,
                 extractionLevel: extractionType.COMUNAL,
                 url: URL,
-            } as TasaPobreza;
+            };
+            const tasPob = new TasaPobreza(data);
+            tasPob.save();
+            return data;
         }).get();
 
-        poblCarArr = $(tables[1]).find("tr").map((_idx, elem) => {
+        poblCarArr = $(tables[1]).find("tr").map(async (_idx, elem) => {
             const tds = $(elem).find("td");
             const uniTer = $(tds[0]).text().trim();
             const perCar = $(tds[1]).text().trim();
@@ -46,14 +55,17 @@ async function main() {
             if (uniTer === "" || perCar === "" || hogHac === "") return;
             const personasCarentes = stringToNumber(perCar);
             const hogaresHacinados = stringToNumber(hogHac);
-            return { 
+            const data: IPoblacionCarente = {
                 unidadTerritorial: uniTer,
                 personasCarentes, 
                 hogaresHacinados,
                 date,
                 extractionLevel: extractionType.COMUNAL,
                 url: URL,
-            } as PoblacionCarente;
+            };
+            const poblCar = new PoblacionCarente(data);
+            await poblCar.save();
+            return data;
         }).get();
 
     });
@@ -64,6 +76,7 @@ async function main() {
     tasPobArrCsv.save("tasa");
     poblCarArrCsv.save("poblacion");
 }
+
 
 const stringToNumber = (textNumber: string): Number => {
     const replaceCommaToDot = textNumber.replace(",", ".");
---
+++ src/config/Database.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import Config from "./Config";
+
+class Database {
+    private constructor(){}
+    static async connect(){
+        const config = new Config();
+        try{
+            await mongoose.connect(config.DB_URL);
+        }
+        catch(error){
+            throw error;
+        }
+        console.log(`DB connected on port ${config.DB_PORT}`);
+    }
+}
+
+export default Database;
