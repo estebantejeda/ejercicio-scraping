941497a483ea73b2325118dd7cff37002356a8f3
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-02-23 12:15:34 -0300

    Creado modelos para las interfaces

---
+++ src/Models/PoblacionCarente.ts
@@ -0,0 +1,25 @@
+import {Schema, model} from "mongoose";
+import IPoblacionCarente from "../interfaces/PoblacionCarente";
+import extractionType from "../enums/ExtractionType";
+
+const PoblacionCarente = new Schema<IPoblacionCarente>({
+    url: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: new Date()
+    },
+    extractionLevel: {
+        type: String,
+        enum: Object.values(extractionType),
+        required: true
+    },
+    unidadTerritorial: String,
+    personasCarentes: Number,
+    hogaresHacinados: Number
+});
+
+export default model <IPoblacionCarente>("poblacion_carente", PoblacionCarente);
---
+++ src/Models/TasaPobreza.ts
@@ -0,0 +1,25 @@
+import {Schema, model} from "mongoose";
+import extractionType from "../enums/ExtractionType";
+import ITasaPobreza from "../interfaces/TasaPobreza";
+
+const TasaPobreza = new Schema<ITasaPobreza>({
+    url: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: new Date()
+    },
+    extractionLevel: {
+        type: String,
+        enum: Object.values(extractionType),
+        required: true
+    },
+    unidadTerritorial: String,
+    porIngresos: Number,
+    multidimensional: Number
+});
+
+export default model <ITasaPobreza>("tasa_pobreza", TasaPobreza);
