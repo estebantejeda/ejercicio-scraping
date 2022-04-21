e1253e24811193f8c239fd5ea7ad4f5ebcd63697
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-04-21 16:11:52 -0400

    utils/Save.ts: Cambiado nombre de método saveCsv(...) a toCsv(...). Además se creó el método toHtml(...) que permite transformar un string de formato html a un fichero

--- src/utils/Save.ts
+++ src/utils/Save.ts
@@ -2,13 +2,22 @@ import path from "path";
 import fs from "fs";
 
 class Save {
-	static saveCsv(data: string, name?: string) {
+	static toCsv(data: string, name?: string) {
 		name = name || "data2csv";
 		const dir = path.resolve(__dirname, "../public");
 		const file = `${dir}/${name}.csv`;
 		if(!fs.existsSync(dir)) fs.mkdirSync(dir);
 		fs.writeFileSync(file, data);
 		console.log("CSV saved");
+	}
+
+	static toHtml(data: string, name?: string) {
+		name = name || "page";
+		const dir = path.resolve(__dirname, "../public");
+		const file = `${dir}/${name}.html`;
+		if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+		fs.writeFileSync(file, data);
+		console.log("HTML saved");
 	}
 }
 
