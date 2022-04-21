b79cfdcca9b2abde828d28afe89a6087f1378301
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-04-21 15:22:50 -0400

    config/Database.ts: Separa lógica en variables de configuración. Database.ts ahora solo contendrá las variables de entorno que respecten a la base de datos

--- src/config/Database.ts
+++ src/config/Database.ts
@@ -1,19 +1,42 @@
-import mongoose from "mongoose";
-import Config from "./Config";
+import * as dotenv from "dotenv";
+import path from "path";
 
 class Database {
-    private constructor(){}
+	readonly #DB_USER: string;
+	readonly #DB_PASS: string;
+	readonly #DB_HOST: string;
+	readonly #DB_PORT: string;
+	readonly #DB_NAME: string;
 
-    static async connect(){
-        const config = new Config();
-        try{
-            await mongoose.connect(config.DB_URL);
-        }
-        catch(error){
-            throw error;
-        }
-        console.log(`DB ${config.DB_NAME} connected on port ${config.DB_PORT}`);
-    }
+	constructor() {
+		dotenv.config({path: path.resolve(__dirname, "../../.env")});
+		this.#DB_USER = process.env["DB_USER"] || "root";
+		this.#DB_PASS = process.env["DB_PASS"] || "toor";
+		this.#DB_HOST =  process.env["DB_HOST"] || "127.0.0.1";
+		this.#DB_PORT = process.env["DB_PORT"] || "27017";
+		this.#DB_NAME = process.env["DB_NAME"] ||"resiliencia";
+	}
+
+	get DB_USER() {
+		return this.#DB_USER;
+	}
+
+	get DB_PASS() {
+		return this.#DB_PASS;
+	}
+
+	get DB_HOST() {
+		return this.#DB_HOST;
+	}
+
+	get DB_PORT() {
+		return this.#DB_PORT;
+	}
+
+	get DB_NAME() {
+		return this.#DB_NAME;
+	}
+
 }
 
 export default Database;
