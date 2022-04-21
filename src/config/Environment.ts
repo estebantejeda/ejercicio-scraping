02f8c0e1425bfddb96430ca5db8b0257a1a8aa6a
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-04-21 15:23:47 -0400

    config/Environment.ts: Ahora solo se encargará de las variables de entorno generales, dejando las variables específicas de la base de datos al archivo Database.ts

--- src/config/Environment.ts
+++ src/config/Environment.ts
@@ -1,48 +1,25 @@
-import * as dotenv from "dotenv";
 import path from "path";
+import NodeEnvType from "../enums/NodeEnvType";
+import * as dotenv from "dotenv";
 
-class Config{
-    readonly #DB_USER: string;
-    readonly #DB_PASS: string;
-    readonly #DB_HOST: string;
-    readonly #DB_PORT: string;
-    readonly #DB_NAME: string;
-    readonly #DB_URL: string;
+class Environment {
+	readonly #PORT: number;
+	readonly #NODE_ENV: NodeEnvType;
 
-    constructor(){
-        dotenv.config({path: path.resolve(__dirname, "../../.env")});
-        this.#DB_USER = process.env["DB_USER"] || "root";
-        this.#DB_PASS = process.env["DB_PASS"] || "toor";
-        this.#DB_HOST =  process.env["DB_HOST"] || "127.0.0.1";
-        this.#DB_PORT = process.env["DB_PORT"] || "27017";
-        this.#DB_NAME = process.env["DB_NAME"] ||"resiliencia";
-        this.#DB_URL = `mongodb://${this.DB_USER}:${this.DB_PASS}@${this.DB_HOST}:${this.DB_PORT}/${this.DB_NAME}`;
-    }
+	constructor() {
+		dotenv.config({path: path.resolve(__dirname, "../../.env")});
+		this.#PORT = parseInt(process.env["PORT"]!) || 3000;
+		this.#NODE_ENV = NodeEnvType.PRODUCTION;
+	}
 
-    get DB_USER(){
-        return this.#DB_USER;
-    }
+	get PORT() {
+		return this.#PORT;
+	}
 
-    get DB_PASS(){
-        return this.#DB_PASS;
-    }
-
-    get DB_HOST(){
-        return this.#DB_HOST;
-    }
-
-    get DB_PORT(){
-        return this.#DB_PORT;
-    }
-
-    get DB_NAME(){
-        return this.#DB_NAME;
-    }
-
-    get DB_URL(){
-        return this.#DB_URL;
-    }
+	get NODE_ENV() {
+		return this.#NODE_ENV;
+	}
     
 }
 
-export default Config;
+export default Environment;
